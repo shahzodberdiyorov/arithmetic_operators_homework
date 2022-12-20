acc3152cc6766c6419fc895ba5c11bc30b4e68a9
shahzodberdiyorov <shahzodberdiyorov30@gmail.com>
2022-12-20 16:34:35 +0500

    03

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,8 +1,9 @@
 #Create a variable called 'number' and assign it the two-digit number
 number=37
 #Find the reverse of the number and assign it to a variable called 'answer'.
-x1=number%10*10
-x2=number//10
+
 
 #Print the value of the 'answer'
+x1=number%10*10
+x2=number//10
 print(x1+x2)
